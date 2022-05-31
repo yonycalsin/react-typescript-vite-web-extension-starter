860268d514521f9b9dea4c4b0b4eab82bd79a006
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-30 22:12:27 -0500

    chore: improve the vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,44 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 import { isDev, resolvePath } from './scripts/utils'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: resolvePath('src'),
-  resolve: {
-    alias: {
-      '~/': resolvePath('src'),
+export default defineConfig((config): UserConfig => {
+  const plugins: PluginOption[] = [react()]
+
+  if (config.command === 'build') {
+    plugins.push(
+      createHtmlPlugin({
+        minify: true,
+      }),
+    )
+  }
+
+  return {
+    plugins,
+    root: resolvePath('src'),
+    base: '/dist/',
+    define: {
+      __DEV__: isDev,
     },
-  },
-  define: {
-    __DEV__: isDev,
-  },
-  base: '/dist/',
-  build: {
-    outDir: resolvePath('extension/dist'),
-    emptyOutDir: true,
-    sourcemap: isDev ? 'inline' : false,
-    rollupOptions: {
-      input: {
-        background: resolvePath('src/background/index.html'),
-        options: resolvePath('src/options/index.html'),
-        popup: resolvePath('src/popup/index.html'),
+    resolve: {
+      alias: {
+        '~/': resolvePath('src'),
       },
     },
-  },
-  plugins: [react()],
+    build: {
+      outDir: resolvePath('extension/dist'),
+      emptyOutDir: true,
+      sourcemap: isDev ? 'inline' : false,
+      rollupOptions: {
+        input: {
+          background: resolvePath('src/background/index.html'),
+          options: resolvePath('src/options/index.html'),
+          popup: resolvePath('src/popup/index.html'),
+        },
+      },
+    },
+  }
 })
